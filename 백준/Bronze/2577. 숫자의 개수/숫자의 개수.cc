cb3a2e4eb1c83c04cac81af3763d19809022ee17
손현경 <shkisme0130@gmail.com>
2023-07-09 00:20:42 +0900

    Update 숫자의 개수.cc

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,25 +1,31 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
+int d[10];
+
 int main() {
-	int num1, num2, num3;
-	int result; // 값들의 곱
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a, b, c;
 
-	int cnt[10] = {}; 
+    cin >> a >> b >> c;
 
+    int mul = a * b * c;
 
-	cin >> num1 >> num2 >> num3;
+    string result = to_string(mul);
 
-	result = num1 * num2 * num3;
+    for (int i = 0 ; i < 10; i++){
+        for (char c : result){
+            if (c - '0' == i){
+                d[i]++;
+            }
+        }
+    }
 
-	while (result > 0) {
-		cnt[result % 10]++;
-		result /= 10;
-	}
+    for (int i = 0; i < 10; i++){
+        cout << d[i] << "\n";
+    }
 
-	for (int num : cnt)
-		cout << num << "\n";
-
-	return 0;
+    return 0;
 }
