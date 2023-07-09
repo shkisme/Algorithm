0edd134e1dc945890992ce8c680f7c325af57397
손현경 <shkisme0130@gmail.com>
2023-07-09 11:14:04 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -13,26 +13,18 @@ int main() {
         string str1, str2;
         cin >> str1 >> str2;
 
-        if (str1.length() != str2.length()) {
-            cout << "Impossible\n";
-            continue;
-        }
-
-        int c1[26], c2[26];
-
-        fill(c1, c1 + 26, 0);
-        fill(c2, c2 + 26, 0);
+        int arr[26] = {};
 
         for (char c: str1) {
-            c1[c - 'a']++;
+            arr[c - 'a']++;
         }
         for (char c: str2) {
-            c2[c - 'a']++;
+            arr[c - 'a']--;
         }
 
         bool impossible = true;
-        for (int i = 0; i < 26; i++) {
-            if (c1[i] != c2[i]) {
+        for (int j: arr) {
+            if (arr[j] != 0) {
                 cout << "Impossible\n";
                 impossible = false;
                 break;
