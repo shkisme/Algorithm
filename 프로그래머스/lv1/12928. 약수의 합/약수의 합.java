fbc3aa11e32f8d531604facbd6cfc118326aa58a
손현경 <shkisme0130@gmail.com>
2023-09-28 13:35:42 +0900

    [level 1] Title: 약수의 합, Time: 1.10 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++){
+            if (n % i == 0) list.add(i);
+        }
+        answer = list.stream().mapToInt(Integer::intValue).sum();
+        return answer;
+    }
+}
