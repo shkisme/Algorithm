4a8df34d8c0978620dc0b01e6e4f88d10df2a0e2
손현경 <shkisme0130@gmail.com>
2024-10-14 16:35:48 +0900

    [level 2] Title: 올바른 괄호, Time: 7.07 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,23 +1,31 @@
-import java.util.*;
-
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
         
-        Stack<Character> stack = new Stack<>();
+        char[] stack = new char[100_001];
+        int index = 0;
         
         for (int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            
-            if (c == '(') stack.push(c);
-            else if (c == ')'){
-                if (stack.isEmpty()) return false;
+            if (s.charAt(i) == '('){
+                stack[index++] = '(';
+            } else {
+                if (index == 0){
+                    answer = false;
+                    break;
+                }
                 
-                char p = stack.pop();
+                if (stack[index - 1] == ')'){
+                    answer = false;
+                    break;
+                } else {
+                    index--;
+                }
             }
         }
         
-        if (!stack.isEmpty()) return false;
+        if (index != 0) {
+            answer = false;
+        }
 
         return answer;
     }
