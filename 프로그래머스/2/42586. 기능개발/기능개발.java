a34f2d4cf536aee53199947176542c58df368986
손현경 <shkisme0130@gmail.com>
2024-10-15 12:29:33 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,34 +1,33 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> list = new ArrayList<>();
-        int pointer = 0;
-        
-        while(true){
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        int[] work = new int[progresses.length + 1];
+        for (int i = 0; i < progresses.length; i++){
+            int a = (100 - progresses[i]) / speeds[i];
+            int b = (100 - progresses[i]) % speeds[i];
             
-            if (pointer >= speeds.length) break;
-            
-            for (int i = 0 ;i < speeds.length; i++){
-                progresses[i] += speeds[i];
-            }
-            
-            if (progresses[pointer] >= 100){
-                    
-                int cnt = 0;
-
-                for (int j = pointer; j < speeds.length; j++){
-                    if (progresses[j] >= 100){
-                        cnt++;
-                        pointer++;
-                    }
-                    if (progresses[j] < 100) break;
-                }
-                
-                list.add(cnt);
+            if (b > 0) {
+                work[i] = a + 1;
+            } else {
+                work[i] = a;
             }
         }
         
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        List<Integer> list = new ArrayList<>();
+        int mx = work[0];
+        int cnt = 1;
+        for (int i = 1; i < progresses.length; i++){
+            int num = work[i];
+            if (mx >= num){
+                cnt++;
+            } else {
+                list.add(cnt);
+                cnt = 1;
+                mx = num;
+            }
+        }
+        list.add(cnt);
+        return list;
     }
 }
