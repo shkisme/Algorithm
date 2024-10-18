da94f0fdee82ddef1a45fdc64bf348949754db8c
손현경 <shkisme0130@gmail.com>
2024-10-18 21:43:02 +0900

    [Silver II] Title: 유기농 배추, Time: 152 ms, Memory: 17440 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,14 +1,10 @@
-import static java.lang.System.in;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 class Pair {
     int x, y;
 
-    public Pair(int x, int y) {
+    public Pair(final int x, final int y) {
         this.x = x;
         this.y = y;
     }
@@ -16,60 +12,63 @@ class Pair {
 
 public class Main {
 
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(java.lang.System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(java.lang.System.out));
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        int t = Integer.parseInt(br.readLine());
 
-        int T = Integer.parseInt(br.readLine());
-        for (int t = 0; t < T; t++) {
+        for (int i = 0; i < t; i++) {
+            String[] strings = br.readLine().split(" ");
+            int m = Integer.parseInt(strings[0]);
+            int n = Integer.parseInt(strings[1]);
+            int k = Integer.parseInt(strings[2]);
 
-            String[] token = br.readLine().split(" ");
-            int width = Integer.parseInt(token[0]);
-            int length = Integer.parseInt(token[1]);
-            int cnt = Integer.parseInt(token[2]);
+            int[][] board = new int[n + 1][m + 1];
+            for (int j = 0; j < k; j++) {
+                strings = br.readLine().split(" ");
 
-            Queue<Pair> pairs = new LinkedList<>();
-            boolean[][] isVisit = new boolean[length][width];
-            int[][] arr = new int[length][width];
+                int x = Integer.parseInt(strings[0]);
+                int y = Integer.parseInt(strings[1]);
 
-            for (int i = 0; i < cnt; i++) {
-                token = br.readLine().split(" ");
-                int y = Integer.parseInt(token[0]); // 가로
-                int x = Integer.parseInt(token[1]); // 세로
-                arr[x][y] = 1;
+                board[y][x] = 1;
             }
 
-            int ans = 0;
-            for (int i = 0; i < length; i++) {
-                for (int j = 0; j < width; j++) {
+            int cnt = 0;
+            Queue<Pair> q = new LinkedList<>();
+            boolean[][] visit = new boolean[n + 1][m + 1];
+            int[] dx = {0, 0, 1, -1};
+            int[] dy = {1, -1, 0, 0};
 
-                    if (arr[i][j] == 1 && !isVisit[i][j]) {
-                        isVisit[i][j] = true;
-                        pairs.offer(new Pair(i, j));
-                        ans++;
-                        while (!pairs.isEmpty()) {
-                            Pair poll = pairs.poll();
+            for (int j = 0; j < n; j++) {
+                for (int z = 0; z < m; z++) {
+                    if (board[j][z] == 0 || visit[j][z]) {
+                        continue;
+                    }
 
-                            for (int k = 0; k < 4; k++) {
-                                int nx = poll.x + dx[k];
-                                int ny = poll.y + dy[k];
+                    q.offer(new Pair(j, z));
+                    visit[j][z] = true;
+                    cnt++;
 
-                                if (nx < 0 || ny < 0 || nx >= length || ny >= width || isVisit[nx][ny]
-                                        || arr[nx][ny] == 0) {
-                                    continue;
-                                }
-                                isVisit[nx][ny] = true;
-                                pairs.offer(new Pair(nx, ny));
+                    while (!q.isEmpty()) {
+                        Pair poll = q.poll();
+
+                        for (int d = 0; d < 4; d++) {
+                            int nx = dx[d] + poll.x;
+                            int ny = dy[d] + poll.y;
+
+                            if (nx < 0 || nx >= n || ny < 0 || ny >= m || board[nx][ny] == 0 || visit[nx][ny]) {
+                                continue;
                             }
+
+                            q.offer(new Pair(nx, ny));
+                            visit[nx][ny] = true;
                         }
                     }
                 }
             }
-
-            System.out.println(ans);
-
+            bw.write(cnt + "\n");
         }
+        bw.flush();
     }
 }
