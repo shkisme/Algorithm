e244c4f3515f3013de05c827f37ca5ab6458d2b8
손현경 <shkisme0130@gmail.com>
2024-10-28 19:24:19 +0900

    [Gold V] Title: 적록색약, Time: 148 ms, Memory: 17108 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,113 +1,164 @@
-import static java.lang.System.in;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 class Pair {
     int x, y;
-    char value;
 
-    public Pair(int x, int y, char value) {
+    public Pair(final int x, final int y) {
         this.x = x;
         this.y = y;
-        this.value = value;
     }
 }
 
 public class Main {
 
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(java.lang.System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(java.lang.System.out));
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        int n = Integer.parseInt(br.readLine());
+        char[][] board = new char[n + 1][n + 1];
 
-        int N = Integer.parseInt(br.readLine());
-        Character[][] arr = new Character[N][N];
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = s.charAt(j);
+        for (int i = 0; i < n; i++) {
+            String string = br.readLine();
+            for (int j = 0; j < n; j++) {
+                board[i][j] = string.charAt(j);
             }
         }
 
-        Queue<Pair> pairs = new LinkedList<>();
-        boolean[][] isVisit = new boolean[N][N];
+        Queue<Pair> q1 = new LinkedList<>();
+        Queue<Pair> q2 = new LinkedList<>();
 
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!isVisit[i][j]) {
-                    isVisit[i][j] = true;
-                    pairs.offer(new Pair(i, j, arr[i][j]));
-                    cnt++;
-                    while (!pairs.isEmpty()) {
-                        Pair poll = pairs.poll();
+        boolean[][] visit1 = new boolean[n + 1][n + 1];
+        boolean[][] visit2 = new boolean[n + 1][n + 1];
 
-                        char p = poll.value;
-                        for (int k = 0; k < 4; k++) {
-                            int nx = poll.x + dx[k];
-                            int ny = poll.y + dy[k];
+        int cnt1 = 0;
+        int cnt2 = 0;
 
-                            if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                char charAt = board[i][j];
+
+                if (charAt == 'R' && !visit1[i][j]) {
+                    q1.offer(new Pair(i, j));
+                    visit1[i][j] = true;
+                    cnt1++;
+
+                    while (!q1.isEmpty()) {
+                        Pair poll = q1.poll();
+
+                        for (int d = 0; d < 4; d++) {
+                            int nx = dx[d] + poll.x;
+                            int ny = dy[d] + poll.y;
+
+                            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
                                 continue;
                             }
-                            if (isVisit[nx][ny] || arr[nx][ny] != p) {
+                            if (board[nx][ny] != 'R' || visit1[nx][ny]) {
                                 continue;
                             }
-                            isVisit[nx][ny] = true;
-                            pairs.offer(new Pair(nx, ny, arr[nx][ny]));
+                            q1.offer(new Pair(nx, ny));
+                            visit1[nx][ny] = true;
+                        }
+                    }
+                } else if (charAt == 'G' && !visit1[i][j]) {
+                    q1.offer(new Pair(i, j));
+                    visit1[i][j] = true;
+                    cnt1++;
+
+                    while (!q1.isEmpty()) {
+                        Pair poll = q1.poll();
+
+                        for (int d = 0; d < 4; d++) {
+                            int nx = dx[d] + poll.x;
+                            int ny = dy[d] + poll.y;
+
+                            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                                continue;
+                            }
+                            if (board[nx][ny] != 'G' || visit1[nx][ny]) {
+                                continue;
+                            }
+                            q1.offer(new Pair(nx, ny));
+                            visit1[nx][ny] = true;
+                        }
+                    }
+                } else if (charAt == 'B' && !visit1[i][j]) {
+                    q1.offer(new Pair(i, j));
+                    visit1[i][j] = true;
+                    cnt1++;
+
+                    while (!q1.isEmpty()) {
+                        Pair poll = q1.poll();
+
+                        for (int d = 0; d < 4; d++) {
+                            int nx = dx[d] + poll.x;
+                            int ny = dy[d] + poll.y;
+
+                            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                                continue;
+                            }
+                            if (board[nx][ny] != 'B' || visit1[nx][ny]) {
+                                continue;
+                            }
+                            q1.offer(new Pair(nx, ny));
+                            visit1[nx][ny] = true;
+                        }
+                    }
+                }
+
+                if ((charAt == 'R' || charAt == 'G') && !visit2[i][j]) {
+                    q2.offer(new Pair(i, j));
+                    visit2[i][j] = true;
+                    cnt2++;
+
+                    while (!q2.isEmpty()) {
+                        Pair poll = q2.poll();
+
+                        for (int d = 0; d < 4; d++) {
+                            int nx = dx[d] + poll.x;
+                            int ny = dy[d] + poll.y;
+
+                            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                                continue;
+                            }
+                            if (board[nx][ny] == 'B' || visit2[nx][ny]) {
+                                continue;
+                            }
+                            q2.offer(new Pair(nx, ny));
+                            visit2[nx][ny] = true;
+                        }
+                    }
+                } else if (charAt == 'B' && !visit2[i][j]) {
+                    q2.offer(new Pair(i, j));
+                    visit2[i][j] = true;
+                    cnt2++;
+
+                    while (!q2.isEmpty()) {
+                        Pair poll = q2.poll();
+
+                        for (int d = 0; d < 4; d++) {
+                            int nx = dx[d] + poll.x;
+                            int ny = dy[d] + poll.y;
+
+                            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                                continue;
+                            }
+                            if (board[nx][ny] != 'B' || visit2[nx][ny]) {
+                                continue;
+                            }
+                            q2.offer(new Pair(nx, ny));
+                            visit2[nx][ny] = true;
                         }
                     }
                 }
             }
         }
-        System.out.print(cnt + " ");
 
-        cnt = 0;
-        boolean[][] isVisit2 = new boolean[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!isVisit2[i][j]) {
-                    isVisit2[i][j] = true;
-                    pairs.offer(new Pair(i, j, arr[i][j]));
-                    cnt++;
-                    while (!pairs.isEmpty()) {
-                        Pair poll = pairs.poll();
-
-                        char p = poll.value;
-                        for (int k = 0; k < 4; k++) {
-                            int nx = poll.x + dx[k];
-                            int ny = poll.y + dy[k];
-
-                            if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
-                                continue;
-                            }
-                            if (isVisit2[nx][ny]) {
-                                continue;
-                            }
-                            if (p == 'R') {
-                                if (arr[nx][ny] != 'G' && arr[nx][ny] != p) {
-                                    continue;
-                                }
-                            } else if (p == 'G') {
-                                if (arr[nx][ny] != 'R' && arr[nx][ny] != p) {
-                                    continue;
-                                }
-                            } else {
-                                if (arr[nx][ny] != p) {
-                                    continue;
-                                }
-                            }
-                            isVisit2[nx][ny] = true;
-                            pairs.offer(new Pair(nx, ny, arr[nx][ny]));
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println(cnt);
+        bw.write(cnt1 + " " + cnt2);
+        bw.flush();
     }
 }
