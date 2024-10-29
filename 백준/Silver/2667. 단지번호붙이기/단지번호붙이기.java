5704d63f79781bd9af454160403e6104ce2fe021
손현경 <shkisme0130@gmail.com>
2024-10-29 20:45:22 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,83 +1,76 @@
-import static java.lang.System.in;
-import static java.lang.System.out;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 class Pair {
     int x, y;
 
-    public Pair(int x, int y) {
+    public Pair(final int x, final int y) {
         this.x = x;
         this.y = y;
     }
 }
 
+public class Main {
 
-class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(java.lang.System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(java.lang.System.out));
 
     public static void main(String[] args) throws Exception {
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int[][] arr = new int[N][N];
+        int[][] board = new int[n + 1][n + 1];
+        boolean[][] visit = new boolean[n + 1][n + 1];
 
-        for (int i = 0; i < N; i++) {
-            String read = br.readLine();
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = read.charAt(j) - '0';
+        for (int i = 0; i < n; i++) {
+            String string = br.readLine();
+            for (int j = 0; j < n; j++) {
+                board[i][j] = string.charAt(j) - '0';
             }
         }
 
-        Queue<Pair> queue = new LinkedList<>();
-        boolean[][] isVisit = new boolean[N][N];
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        int cnt = 0;
+        Queue<Pair> q = new LinkedList<>();
         List<Integer> list = new ArrayList<>();
+        int cnt = 0;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (arr[i][j] == 1 && !isVisit[i][j]) {
-                    isVisit[i][j] = true;
-                    queue.add(new Pair(i, j));
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1 && !visit[i][j]) {
+                    q.offer(new Pair(i, j));
+                    visit[i][j] = true;
                     cnt++;
 
                     int tmp = 0;
-                    while (!queue.isEmpty()) {
-                        Pair poll = queue.poll();
+
+                    while (!q.isEmpty()) {
+                        Pair poll = q.poll();
                         tmp++;
 
-                        for (int k = 0; k < 4; k++) {
-                            int nx = poll.x + dx[k];
-                            int ny = poll.y + dy[k];
+                        for (int d = 0; d < 4; d++) {
+                            int nx = dx[d] + poll.x;
+                            int ny = dy[d] + poll.y;
 
-                            if (nx < 0 || ny < 0 || nx >= N || ny >= N || isVisit[nx][ny] || arr[nx][ny] == 0) {
+                            if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
                                 continue;
                             }
-                            isVisit[nx][ny] = true;
-                            queue.offer(new Pair(nx, ny));
+                            if (board[nx][ny] == 0 || visit[nx][ny]) {
+                                continue;
+                            }
+
+                            visit[nx][ny] = true;
+                            q.offer(new Pair(nx, ny));
                         }
                     }
                     list.add(tmp);
                 }
             }
         }
-
         bw.write(cnt + "\n");
-        Collections.sort(list);
-        for (int l : list) {
-            bw.write(l + "\n");
+        list.sort(Comparator.naturalOrder());
+        for (int i = 0; i < list.size(); i++) {
+            bw.write(list.get(i) + "\n");
         }
         bw.flush();
     }
