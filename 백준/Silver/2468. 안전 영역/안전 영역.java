c8a89498050178203babf4e187f14f2e29ee93b5
손현경 <shkisme0130@gmail.com>
2024-11-06 12:37:55 +0900

    [Silver I] Title: 안전 영역, Time: 288 ms, Memory: 56636 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,17 +1,10 @@
-import static java.lang.System.in;
-import static java.lang.System.out;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 class Pair {
     int x, y;
 
-    public Pair(int x, int y) {
+    public Pair(final int x, final int y) {
         this.x = x;
         this.y = y;
     }
@@ -19,67 +12,64 @@ class Pair {
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(java.lang.System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(java.lang.System.out));
 
     public static void main(String[] args) throws Exception {
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int[][] arr = new int[N][N];
+        int[][] board = new int[n + 1][n + 1];
 
-        int maxValue = Integer.MIN_VALUE;
-
-        for (int i = 0; i < N; i++) {
-            String[] read = br.readLine().split(" ");
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(read[j]);
-                if (maxValue < arr[i][j]) {
-                    maxValue = arr[i][j];
-                }
+        for (int i = 0; i < n; i++) {
+            String[] strings = br.readLine().split(" ");
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(strings[j]);
             }
         }
 
-        int answer = Integer.MIN_VALUE;
+        Queue<Pair> q = new LinkedList<>();
 
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        for (int i = 0; i <= maxValue; i++) {
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
 
+        int mx = 1;
+
+        for (int k = 1; k <= 100; k++) {
             int cnt = 0;
-            Queue<Pair> queue = new LinkedList<>();
-            boolean[][] isVisit = new boolean[N][N];
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < N; k++) {
-                    if (i < arr[j][k] && !isVisit[j][k]) {
-                        queue.offer(new Pair(j, k));
-                        isVisit[j][k] = true;
+            boolean[][] visit = new boolean[n + 1][n + 1];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (board[i][j] > k && !visit[i][j]) {
+                        q.offer(new Pair(i, j));
+                        visit[i][j] = true;
                         cnt++;
 
-                        while (!queue.isEmpty()) {
-                            Pair poll = queue.poll();
+                        while (!q.isEmpty()) {
+                            Pair poll = q.poll();
 
-                            for (int l = 0; l < 4; l++) {
-                                int nx = poll.x + dx[l];
-                                int ny = poll.y + dy[l];
+                            for (int d = 0; d < 4; d++) {
+                                int nx = dx[d] + poll.x;
+                                int ny = dy[d] + poll.y;
 
-                                if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
+                                if (nx < 0 || ny < 0 || nx >= n || ny >= n) {
                                     continue;
                                 }
-                                if (isVisit[nx][ny] || arr[nx][ny] <= i) {
+                                if (board[nx][ny] <= k || visit[nx][ny]) {
                                     continue;
                                 }
-                                queue.offer(new Pair(nx, ny));
-                                isVisit[nx][ny] = true;
+                                q.offer(new Pair(nx, ny));
+                                visit[nx][ny] = true;
                             }
                         }
                     }
                 }
             }
-            if (answer < cnt) {
-                answer = cnt;
-            }
+
+            mx = Math.max(mx, cnt);
         }
-        bw.write(answer + "");
+
+        bw.write(mx + "");
         bw.flush();
     }
 }
