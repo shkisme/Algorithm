1dfe76ce8a12cd3d949f35a411e3570cb6f2421c
손현경 <shkisme0130@gmail.com>
2024-11-06 14:45:39 +0900

    [Silver III] Title: N과 M (1), Time: 268 ms, Memory: 30536 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,39 +1,39 @@
-import static java.lang.System.in;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(java.lang.System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(java.lang.System.out));
 
-    static int N = 0, M = 0;
-
+    static int N, M;
     static int[] arr = new int[10];
     static boolean[] isUsed = new boolean[10];
 
     public static void main(String[] args) throws Exception {
-        String[] read = br.readLine().split(" ");
-        N = Integer.parseInt(read[0]);
-        M = Integer.parseInt(read[1]);
+        String[] strings = br.readLine().split(" ");
+        N = Integer.parseInt(strings[0]);
+        M = Integer.parseInt(strings[1]);
 
-        func(0);
+        fun(1);
+        bw.flush();
     }
 
-    private static void func(int num) { // 현재까지 K개의 수를 택함.
-        if (num == M) { // M개를 모두 택했다면
-            for (int i = 0; i < M; i++) {
-                System.out.print(arr[i] + " ");
+    private static void fun(final int num) throws IOException {
+        if (num == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                bw.write(arr[i] + " ");
             }
-            System.out.println();
+            bw.write("\n");
             return;
         }
-        for (int i = 1; i <= N; i++) { // 1부터 n개까지 수에 대해서,
-            if (!isUsed[i]) { // 아직 사용되지 않았다면,
-                arr[num] = i; // K번째 수를 i로 정함.
-                isUsed[i] = true; // i를 사용했다고 표시
-                func(num + 1); // 다음 수를 정하러 한 단계 더 들어감.
-                isUsed[i] = false; // k번째 수를 i로 정한 모든 경우에 대해서 다 확인했으니, i를 이제 사용되지 않았다고 명시함.
+
+        for (int i = 1; i <= N; i++) {
+            if (!isUsed[i]) {
+                arr[num] = i;
+                isUsed[i] = true;
+                fun(num + 1);
+                isUsed[i] = false;
             }
         }
     }
