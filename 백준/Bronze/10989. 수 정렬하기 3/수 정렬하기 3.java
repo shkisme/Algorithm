f6ee9f6563cfc10759790e2d12aec91e170a2faa
손현경 <shkisme0130@gmail.com>
2024-11-13 12:14:10 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2496 ms, Memory: 347124 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(java.lang.System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(java.lang.System.out));
+
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            bw.write(arr[i] + "\n");
+        }
+        bw.flush();
+    }
+}
