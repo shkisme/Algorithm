b70e440a51816d21c40cfde77cd73db71474c648
손현경 <shkisme0130@gmail.com>
2024-12-03 16:37:15 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18284 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,48 +1,25 @@
-import static java.lang.System.in;
-import static java.lang.System.out;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Comparator;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
-
-    int[] nums = new int[1000001];
-    nums[2] = 1;
-    nums[3] = 1;
-
-    int input = Integer.parseInt(br.readLine());
-
-    for (int i = 4; i <= input; i++) {
-      if (i % 2 == 0 && i % 3 == 0) {
-        int n1 = nums[i / 2] + 1;
-        int n2 = nums[i / 3] + 1;
-        int n3 = nums[i - 1] + 1;
-
-        nums[i] = List.of(n1, n2, n3).stream()
-            .min(Comparator.naturalOrder())
-            .orElse(null);
-      } else if (i % 2 == 0) {
-        nums[i] = nums[i / 2] < nums[i - 1] ? nums[i / 2] + 1 : nums[i - 1] + 1;
-      } else if (i % 3 == 0) {
-        nums[i] = nums[i / 3] < nums[i - 1] ? nums[i / 3] + 1 : nums[i - 1] + 1;
-      } else {
-        nums[i] = nums[i - 1] + 1;
-      }
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[1000_100];
+        arr[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + 1;
+            if (i % 2 == 0) {
+                arr[i] = Math.min(arr[i], arr[i / 2] + 1);
+            }
+            if (i % 3 == 0) {
+                arr[i] = Math.min(arr[i], arr[i / 3] + 1);
+            }
+        }
+        bw.write(arr[n] + "");
+        bw.flush();
     }
-
-    bw.write(String.valueOf(nums[input]));
-
-    bw.flush();
-    br.close();
-    bw.close();
-  }
 }
