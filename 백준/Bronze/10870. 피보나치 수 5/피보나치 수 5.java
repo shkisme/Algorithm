166da2e9318f1a6a2aa9b3065de9e2e483f0b891
손현경 <shkisme0130@gmail.com>
2024-12-03 16:43:19 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[30];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        bw.write(arr[n] + "");
+        bw.flush();
+    }
+}
