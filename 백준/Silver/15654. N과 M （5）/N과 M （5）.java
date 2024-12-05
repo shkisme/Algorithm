67a58a3d9aac18a14aae5314ff0a97f248ceb3c8
손현경 <shkisme0130@gmail.com>
2024-12-05 17:51:54 +0900

    [Silver III] Title: N과 M (5), Time: 336 ms, Memory: 35656 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,61 +1,50 @@
-import static java.lang.System.in;
-import static java.lang.System.out;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.Arrays;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int N = 0, M = 0;
-
-    static int[] arr = new int[10];
-
-    static int[] save;
-    static boolean[] isVisit;
+    static int N, M;
+    static int[] arr;
+    static int[] tmp;
+    static boolean[] visit;
 
     public static void main(String[] args) throws Exception {
-        String[] read = br.readLine().split(" ");
-        N = Integer.parseInt(read[0]);
-        M = Integer.parseInt(read[1]);
+        String[] strings = br.readLine().split(" ");
+        N = Integer.parseInt(strings[0]);
+        M = Integer.parseInt(strings[1]);
 
-        read = br.readLine().split(" ");
-
-        save = new int[N];
-        isVisit = new boolean[N];
-
+        arr = new int[N];
+        tmp = new int[N];
+        visit = new boolean[N];
+        strings = br.readLine().split(" ");
         for (int i = 0; i < N; i++) {
-            save[i] = Integer.parseInt(read[i]);
+            arr[i] = Integer.parseInt(strings[i]);
         }
 
-        Arrays.sort(save);
-
-        func(0);
+        Arrays.sort(arr);
+        backtracking(0);
         bw.flush();
     }
 
-    private static void func(int num) throws IOException { // 현재까지 K개의 수를 택함.
-        if (num == M) { // M개를 모두 택했다면
+    private static void backtracking(final int num) throws IOException {
+        if (num == M) {
             for (int i = 0; i < M; i++) {
-                bw.write(arr[i] + " ");
+                bw.write(tmp[i] + " ");
             }
             bw.write("\n");
             return;
         }
-        for (int i = 0; i < N; i++) { // 1부터 n개까지 수에 대해서,
-            if (!isVisit[i]) {
-                arr[num] = save[i]; // K번째 수를 i로 정함.
-                isVisit[i] = true;
-                func(num + 1); // 다음 수를 정하러 한 단계 더 들어감.
-                isVisit[i] = false;
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                tmp[num] = arr[i];
+                backtracking(num + 1);
+                visit[i] = false;
             }
         }
     }
 }
-
