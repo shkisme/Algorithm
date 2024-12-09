c441118ce18c3eac643d3dea78ddbf748597867e
손현경 <shkisme0130@gmail.com>
2024-12-09 19:05:19 +0900

    [level 0] Title: 0 떼기, Time: 13.69 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        boolean flag = false;
+        for (int i = 0; i < n_str.length(); i++){
+            if (n_str.charAt(i) != '0' && !flag){
+                answer += n_str.charAt(i);
+                flag = true;
+                continue;
+            }
+            
+            if (flag){
+                answer += n_str.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
