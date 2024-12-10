f5418465f33c6fa8bef1dd77b1bc929cd78ea522
손현경 <shkisme0130@gmail.com>
2024-12-10 17:37:26 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        int tmp = start_num;
+        for (int i = 0; i < end_num - start_num + 1; i++){
+            answer[i] = tmp++;
+        }
+        return answer;
+    }
+}
